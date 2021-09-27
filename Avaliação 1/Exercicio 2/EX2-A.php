84d0b9f132989ed673f7f002e47a2ee6173aedd9
HenriqueMachado046 <71035600+HenriqueMachado046@users.noreply.github.com>
2021-09-26 21:24:51 -0300

    Add files via upload

--- Avaliação 1/Exercicio 2/EX2-A.php
+++ Avaliação 1/Exercicio 2/EX2-A.php
@@ -14,9 +14,8 @@
 
 <body>
 
-  <header class="jumbotron">
-    <h2 class="cinza">Exercício 2</h2>
-    <p class="violeta">	</p>
+  <footer id= "id1" class = "jumbotron">
+    <h2>Exercício 2</h2>
   </header>
 
 	<table width = "95%" border = "1" align = "center">
@@ -25,68 +24,69 @@
 			<div class = "titulo1"> Forms </div><br>
 				<form name = "formulario" action = "EX2-B.php" method = "post"> 
 					
-					<b> Nome </b> <input type = "text" name = "nome"><br> 
-					<b> E-mail: </b> <input type = "text" name = "email"><br>
-					<b> Mensagem: </b><br>
-						<textarea name = "mensagem" cols = "30" rows = "10"></textarea>
+					<b> Insira seu nome </b> <input type = "text" name = "nome"><br> 
+					<b> Seu e-mail: </b> <input type = "text" name = "email"><br>
+					<b> Envie uma mensagem para nós: </b><br>
+						<textarea name = "mensagem" cols = "25" rows = "15"></textarea>
 					<br><hr>  
 						
 					<b> Escolha as suas cores preferidas: </b><br> 
 						<input type = "checkbox" name = "cores[]" value = "Preto"> Preto <br>
-						<input type = "checkbox" name = "cores[]" value = "Verde"> Verde <br>
+						<input type = "checkbox" name = "cores[]" value = "Azul"> Azul <br>
 						<input type = "checkbox" name = "cores[]" value = "Vermelho"> Vermelho <br>
-						<input type = "checkbox" name = "cores[]" value ="Branco"> Branco <br>
+						<input type = "checkbox" name = "cores[]" value ="Rosa"> Rosa <br>
 					<hr>
 						<input type = "checkbox" name = "news" value = "1">
-							<b> Receber Newsletter? </b>
+							<b> Receber notícias no seu email? </b>
 					<br><hr> 
 						
 					<b> Qual seu sistema operacional? </b><br> 
 						<input type = "radio" name = "sistema" value = "Windows"> Windows
 						<input type = "radio" name = "sistema" value = "Linux"> Linux 
-						<input type = "radio" name = "sistema" value = "Mac"> Mac
+						<input type = "radio" name = "sistema" value = "Android"> Android
 					<br><hr> 
 					
 					<b> Qual a marca de seu monitor? </b><br> 
 						<input type = "radio" name = "monitor" value = "Samsung"> Samsung 
 						<input type = "radio" name = "monitor" value = "LG"> LG 
+						<input type = "radio" name = "monitor" value = "Philco"> Philco
 						<input type = "radio" name = "monitor" value = "Outro"> Outro
 					<br><hr>
 						
 					<b> Qual seu processador? </b><br>
 						<select name = "processador">
 							<option value = Pentium> Pentium </option>
-							<option value = AMD> AMD </option>
-							<option value = Celeron> Celeron </option>
+							<option value = Ryzen> Ryzen </option>
+							<option value = Celeron> Celeron III </option>
+							<option value = Series I> Series I </option>
 						</select>
 					<br><hr> 
 					
 					</select>
-					Campo 1: <input type = "text" name = "campo1"><br> 
-					Campo 2: <input type = "text" name = "campo2"><br>
+					Campo de texto teste 1: <input type = "text" name = "campo1"><br> 
+					Campo de texto teste 2: <input type = "text" name = "campo2"><br>
 					<br><hr><br>
 
 					<b> Livros que deseja comprar? </b>
-					<br> <I>Obs: segure "CTRL" para selecionar mais de um.</I><br>
+					<br> Sgure "CTRL" para selecionar mais de um:</I><br>
 						<select multiple name = "livros[]">
-							<option value = "Biblia do PHP 4"> Biblia do PHP 4 </option>
-							<option value = "PHP Professional"> PHP Professional </option>
-							<option value = "Iniciando em PHP"> Iniciando em PHP </option>
-							<option value = "Novidades do PHP 5"> Novidades do PHP 5 </option>
-							<option value = "Biblia do MySQL"> Biblia do MySQL </option>
+							<option value = "Padrões de Projeto"> Padrões de Projeto </option>
+							<option value = "Harry Potter e o Prisioneiro de Azkaban"> Harry Potter e o Prisioneiro de Azkaban </option>
+							<option value = "Senhor dos Aneis 1"> Senhor dos Aneis 1 </option>
+							<option value = "Grande Sertão Veredas"> Grande Sertão Veredas </option>
+							<option value = "Dicionário Houaiss"> Dicionário Houaiss </option>
 						</select>
 					<br><hr>
 
-				<input type = "submit" value = "ENVIAR">
+				<input type = "submit" value = "Enviar">
 				<br><hr>
 				</form> 
 			</td></tr>
 		
 	</table>
 
-    <footer class = "jumbotron">
-    <h3>Renan Gomes Acosta - 11230237</h3>    
-    <h6>"Que a força esteja com você!"</h6>
+    <footer id= "id1" class = "jumbotron">    
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
   </footer>
 
 </body>
